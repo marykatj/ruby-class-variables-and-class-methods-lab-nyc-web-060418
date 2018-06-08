3ad6f693b5e0ee2e557871d93136009a3d6f00ae
Mary-Kate James <marykatj@gmail.com>
2018-06-08 00:30:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
 
 attr_accessor :name, :artist, :genre
 
-@@count = []
+@@count = 0
 @@artists = []
 @@genres = []
 
@@ -10,6 +10,7 @@ def initialize(name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
+  @@count += 1
 end
 
 def new
