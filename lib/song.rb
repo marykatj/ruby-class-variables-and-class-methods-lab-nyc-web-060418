3ea77dc239951b332a64f26641445bed5aee5f55
Mary-Kate James <marykatj@gmail.com>
2018-06-08 00:12:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,25 +1,30 @@
 class Song
 
-  
+
 @@songs = []
 @@artists = []
+@@genres = []
 
-def initialize(song, artist)
+def initialize(song, artist, genre)
   @song = song
   @artist = artist
+  @genre = genre
   @@songs << song.self
   @@artists << artist.self
-
+  @@genres << genre.self
+end
 
 def count
   @@songs.length
 end
 
 def artists
-  @@artists
+  @@artists.uniq
+end
+
+def genres
+  @@genres.uniq
 end
 
 
-
-  
 end
