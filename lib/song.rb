6cb74491faf944f7f6fa724fa782f793894b9b7c
Mary-Kate James <marykatj@gmail.com>
2018-06-08 00:43:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,8 @@ def initialize(name, artist, genre)
   @artist = artist
   @genre = genre
   @@count += 1
-  @@artists << self(1)
-  @@genre << self(2)
+  @@artists << self
+  @@genre << self
 end
 
 def self.count
